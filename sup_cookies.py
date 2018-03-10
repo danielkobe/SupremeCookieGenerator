5eb5b585e63ced381978f9c2a03c86b11c02ef3c
Daniel Koberstein <daniel.koberstein@wsu.edu>
2018-03-10 15:10:47 -0800

    switch to write to file

--- sup_cookies.py
+++ sup_cookies.py
@@ -1,4 +1,3 @@
-
 print("========= Shipping Info =========")
 name = input("Name: ")
 address = input("Address: ")
@@ -9,9 +8,13 @@ country = input("Country (USA or CANADA): ")
 email = input("Email: ")
 phone_number = input("Phone number (no dashes): ")
 
-value = name.replace(" ", "+") + "%7C" + address.replace(" ", "+") + "%7C%7C" + city.replace(" ", "+") + "%7C" + state + "%7C" + zip_code + "%7C" + country + "%7C" + email.replace("@", "%40") + "%7C" + phone_number
+f = open("cookie.txt","w")
 
-cookie = str(json).replace("'", '"').replace("True,", "true,").replace("False,", "false,")
-print("document.cookie=\"address=" + value + "\"")
+value = name.replace(" ", "+") + "%7C" + address.replace(" ", "+") + "%7C%7C" + city.replace(" ", "+") + "%7C" + state + "%7C" + zip_code + "%7C" + country + "%7C" + email.replace("@", "%40") + "%7C" + phone_number
+value = "document.cookie=\"address=" + value + "\""
+
+print(value)
+f.write(value)
 
 print("Cookie generated! First go to http://www.supremenewyork.com/ then to the Chrome Developer Tools->Console, then past the cookie into the console and press enter.")
+fh.close()
